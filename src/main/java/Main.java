f555d9f21aa4b6f3a7f2ed6a7517fb3301b87a29
unknown <manofbackend@fasoo.com>
2021-01-20 12:56:55 +0900

    remove null in csv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,36 @@
-import java.sql.*;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
 
 public class Main {
 
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException, CsvValidationException {
 
+        CSVReader csvReader = new CSVReader(new FileReader("test_1GB.csv"));
+        CSVWriter csvWriter = new CSVWriter(new FileWriter("output_1GB.csv"));
+
+        csvReader.skip(1);
+        Iterator<String[]> iterator = csvReader.iterator();
+        while (iterator.hasNext()) {
+
+            String[] line = iterator.next();
+            for (int i = 0; i < line.length; i++) {
+                if (line[i].isEmpty()) {
+                    line[i] = "0";
+                }
+            }
+            csvWriter.writeNext(line);
+
+        }
+        csvWriter.flush();
+        csvWriter.close();
+        csvReader.close();
 
 
     }
